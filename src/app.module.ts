b3c11336c02e833820e55706e4cab43846c84481
Kiran K K <kirankalak4@gmail.com>
2021-08-22 14:03:24 +0530

    installed mongoose and @nestjs/mongoose set connection to mongodb with help of MongooseClient.ForRoot() command inside the imports of AppModule. Connection setted succesfully. Added a new Schema defined how the structure of my document inside the  collection look like.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './signup-user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,MongooseModule.forRoot('mongodb://localhost/userdb')],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/signup-user/schemas/user-schema.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserDocument = UserSchemaClass & Document;
+
+@Schema()
+export class UserSchemaClass{
+
+    @Prop()
+    fname: string;
+
+    @Prop()
+    lname: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    phoneno: number;
+}
+
+const UserSchema =  SchemaFactory.createForClass(UserSchemaClass);
